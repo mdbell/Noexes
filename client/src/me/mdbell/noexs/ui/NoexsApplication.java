969885b91e6f25e7ecba02ac0f5e929d5a1f48c2
mdbell <contact@mdbell.me>
2018-10-13 19:36:06 -0400

    Increment version number for next release

--- client/src/me/mdbell/noexs/ui/NoexsApplication.java
+++ client/src/me/mdbell/noexs/ui/NoexsApplication.java
@@ -15,7 +15,7 @@ public class NoexsApplication extends Application {
 
     public static final int VERSION_MAJOR = 1;
     public static final int VERSION_MINOR = 1;
-    public static final int VERSION_PATCH = 1;
+    public static final int VERSION_PATCH = 2;
 
     public static final String APP_NAME = "JNoexs";
     public static final String APP_VERSION = VERSION_MAJOR + "." + VERSION_MINOR + "." + VERSION_PATCH;
