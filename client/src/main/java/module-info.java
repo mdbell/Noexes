923d56f4ddd3b3e456cad5c6d105faaed882320e
Pedro Diaz <pedro.diaz@full360.com>
2019-08-27 19:00:14 +0200

    added java.sql module

--- client/src/main/java/module-info.java
+++ client/src/main/java/module-info.java
@@ -1,12 +1,13 @@
 module noexs {
+    requires java.sql;
     requires java.desktop;
     requires java.prefs;
-    requires org.objectweb.asm;
-    requires usb.api;
+    requires javafx.base;
+    requires javafx.graphics;
     requires javafx.controls;
     requires javafx.fxml;
-    requires transitive javafx.base;
-    requires transitive javafx.graphics;
+    requires org.objectweb.asm;
+    requires usb.api;
 
     opens me.mdbell.noexs.ui.controllers to javafx.fxml;
 
